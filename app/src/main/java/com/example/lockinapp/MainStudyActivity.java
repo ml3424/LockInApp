30e9df587fb4017620726e33d3feac61889b1d9e
ml3424 <ml3424@bs.amalnet.k12.il>
2025-12-21 15:48:58 +0200

    MainStudyActivity - start and stop timer TODO: TimerService

--- app/src/main/java/com/example/lockinapp/MainStudyActivity.java
+++ app/src/main/java/com/example/lockinapp/MainStudyActivity.java
@@ -16,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainStudyActivity extends AppCompatActivity {
 
     private SeekBar seekBarTime;
-    private TextView tvSelectedTime, tvUserName;
+    private TextView tvSelectedTime;
     private Button btnToggleTimer;
 
     private String currentUserId;
@@ -28,11 +28,53 @@ public class MainStudyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_study);
 
+        seekBarTime = findViewById(R.id.seekBarTime);
+        tvSelectedTime = findViewById(R.id.tvSelectedTime);
+        btnToggleTimer = findViewById(R.id.btnToggleTimer);
+
+        currentUserId = getIntent().getStringExtra("USER_ID");
+
+        // setup seekbar listener
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // updates the text as the user slides the bar
+                selectedMinutes = progress;
+                tvSelectedTime.setText("Time: " + selectedMinutes + " minutes");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
+
     private void startTimer() {
+        if (selectedMinutes > 0)
+        {
+            // start timer service
+            Intent serviceIntent = new Intent(this, TimerService.class);
+            serviceIntent.putExtra("DURATION_MIN", selectedMinutes);
+            startService(serviceIntent);
+
+            // able stoping the timer
+            isTimerRunning = true;
+            btnToggleTimer.setText("Stop Timer");
+            seekBarTime.setEnabled(false); // lock seekbar
+        }
     }
+
     private void stopTimer() {
+        // stop timer service
+        Intent serviceIntent = new Intent(this, TimerService.class);
+        stopService(serviceIntent);
+
+        // restart timer
+        isTimerRunning = false;
+        btnToggleTimer.setText("Start Timer");
+        seekBarTime.setEnabled(true); // unlock seekbar
     }
 
     public void on_click_log_out(View view) {
